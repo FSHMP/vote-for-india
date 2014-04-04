004feefc2cc36720a48d2770ee28bd9f13cc833a
Prasanna Venkadesh <prasmailme@gmail.com>
2014-04-05 00:50:39 +0530

    removed all the basic files

--- manage.py
+++
@@ -1,4 +0,0 @@
-from voteforindia import app
-
-if __name__ == '__main__':
-    app.run()
--- voteforindia/views.py
+++
@@ -1,8 +0,0 @@
-from voteforindia import app
-from flask import render_template
-
-
-@app.route('/')
-@app.route('/home')
-def homepage():
-    return render_template('index.html')
