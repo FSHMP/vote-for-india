2ec4d1f94002d300c79e9d80abd387d48758a8c5
Prasanna Venkadesh <prasmailme@gmail.com>
2014-04-06 00:25:15 +0530

    Added a AJAX call in template to send data to server and added view function to receive the data from template and return a test status

--- wsgi/voteforindia/views.py
+++ wsgi/voteforindia/views.py
@@ -1,8 +1,14 @@
 from voteforindia import app
-from flask import render_template
+from flask import render_template, request, jsonify
 
 
 @app.route('/')
 @app.route('/home')
 def homepage():
     return render_template('index.html')
+
+
+@app.route('/respond', methods=['POST'])
+def respond():
+    print request.json
+    return jsonify(), 200
