d54416ba7d9618347cfb15d1784c8995d6bd1f3a
Prasanna Venkadesh <prasmailme@gmail.com>
2014-04-05 17:16:48 +0530

    Added configuration file, which will hold the metrics value

---
+++ wsgi/config.py
@@ -0,0 +1,20 @@
+#! coding==utf-8
+
+# a dictionary to maintain range and ranks
+metrics = {
+    'Low': {
+        'range': range(1,8),
+        'rank': 0
+    },
+
+    'Normal': {
+        'range': range(8,13),
+        'rank': 1
+    },
+
+    'High': {
+        'range': range(13,21),
+        'rank': 2
+    }
+
+}
