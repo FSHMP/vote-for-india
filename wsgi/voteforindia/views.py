bc160238e6e3ccbf4c103d461c2c2155c0628779
Vikneshwaren <vikneshwaren.u@gmail.com>
2014-04-06 11:09:02 +0530

    Added favicon

--- wsgi/voteforindia/views.py
+++ wsgi/voteforindia/views.py
@@ -1,5 +1,11 @@
+import os
 from voteforindia import app
 from flask import render_template, request, jsonify
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 @app.route('/')
